e0cf317e5ef396d87b173e336d11a5c0d9f73fca
guapapacho <luisanton999@hotmail.com>
2023-06-14 01:41:50 -0600

    Add files via upload

---
+++ loggin/home.php
@@ -0,0 +1,16 @@
+
+
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>home</title>
+</head>
+
+<body background="UMG.jpeg" bgcolor="FFCECB">
+<h1>BIENVENIDOS A TODOS </h1>
+    
+</body>
+</html>
